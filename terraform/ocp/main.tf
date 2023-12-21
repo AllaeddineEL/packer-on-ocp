4d2069b31d613f5fc77f71006b635d2ab12d76a2
AllaeddineEL <a.elareed@gmail.com>
2023-12-21 13:28:42 +0100

    update demo app deployment

--- terraform/ocp/main.tf
+++ terraform/ocp/main.tf
@@ -8,54 +8,8 @@ data "hcp_packer_image" "path-to-packer-container" {
   cloud_provider = "docker"
   region         = "docker"
 }
-resource "kubernetes_service_account" "demo_app" {
-  metadata {
-    name      = "demo-app"
-    namespace = "demo"
-  }
-  secret {
-    name = kubernetes_secret.demo_app.metadata.0.name
-  }
-}
-
-resource "kubernetes_secret" "demo_app" {
-  metadata {
-    name      = "demo-app"
-    namespace = "demo"
-  }
-}
-resource "kubernetes_manifest" "securitycontextconstraints_demo_app" {
-  manifest = {
-    "allowHostDirVolumePlugin" = true
-    "allowHostNetwork"         = true
-    "allowHostPorts"           = true
-    "allowPrivilegedContainer" = true
-    "apiVersion"               = "security.openshift.io/v1"
-    "defaultAddCapabilities" = [
-      "SYS_ADMIN",
-    ]
-    "fsGroup" = {
-      "type" = "RunAsAny"
-    }
-    "kind" = "SecurityContextConstraints"
-    "metadata" = {
-      "name" = "demo-app"
-    }
-    "runAsUser" = {
-      "type" = "RunAsAny"
-    }
-    "seLinuxContext" = {
-      "type" = "RunAsAny"
-    }
-    "users" = [
-      "system:serviceaccount:${kubernetes_service_account.demo_app.metadata.0.name}",
-    ]
-  }
-
-}
 
 resource "kubernetes_deployment" "demo_app" {
-  depends_on = [kubernetes_manifest.securitycontextconstraints_demo_app]
   metadata {
     name      = "demo-app"
     namespace = "demo"
@@ -86,28 +40,16 @@ resource "kubernetes_deployment" "demo_app" {
         }
       }
       spec {
-        service_account_name = kubernetes_service_account.demo_app.metadata.0.name
         container {
           image = data.hcp_packer_image.path-to-packer-container.labels["ImageDigest"]
           name  = "demo"
-
-          command = [
-            "sh", "-c", "sleep 365"
-          ]
-          security_context {
-            allow_privilege_escalation = false
-            capabilities {
-              drop = [
-                "ALL",
-              ]
-            }
-            run_as_non_root = true
-            run_as_user     = 1000650001
+          port {
+            container_port = 8080
           }
           resources {
             limits = {
               cpu    = "0.5"
-              memory = "64Mi"
+              memory = "512Mi"
             }
             requests = {
               cpu    = "250m"
@@ -115,12 +57,6 @@ resource "kubernetes_deployment" "demo_app" {
             }
           }
         }
-        # security_context {
-        #   fs_group = 1000680000
-        #   seccomp_profile {
-        #     type = "RuntimeDefault"
-        #   }
-        # }
       }
     }
   }
